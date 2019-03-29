1695fc188d61951cde2e357172666b7daafe3cf9
feeewq3 <16137238uca>
2019-03-29 10:58:26 -0600

    avances

--- app/src/main/java/com/example/myapplication/NewActivity.java
+++ app/src/main/java/com/example/myapplication/NewActivity.java
@@ -1,15 +1,25 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myapplication.utils.appConstant;
 
 public class NewActivity extends AppCompatActivity {
+
+    TextView mTv_user, mTv_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        mTv_email=findViewById(R.id.et_email);
+        mTv_user=findViewById(R.id.et_user);
+        Intent mIntent2=getIntent();
 
 
+        }
     }
-}
+
